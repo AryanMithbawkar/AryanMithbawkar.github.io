396ad05741057aa7e36457708e03a674375903d0
AryanMithbawkar <134158945+AryanMithbawkar@users.noreply.github.com>
2025-04-11 10:54:34 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -20,4 +20,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
 
-export { db, collection, addDoc };
+export { db, collection, addDoc, analytics, logEvent }; // Export these
